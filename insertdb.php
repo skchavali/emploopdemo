f3f63e91b715f35a50ad3a491d6491daf9f0de56
skchavali <39451916+skchavali@users.noreply.github.com>
2018-05-21 15:19:53 -0700

    add db

--- insertdb.php
+++ insertdb.php
@@ -3,5 +3,30 @@
 <?php
 
 include 'include.php';
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$servername = $url["host"];
+$username = "b3c0dca2fea3fb";
+$password = "7200888a";
+$dbname = substr($url["path"],1);
 
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO customer (store_id,first_name, last_name,email,address_id,active) VALUES (1,'" . $_POST["firstname"] . "', '" . $_POST["lastname"] . "', '" . $_POST["email"] . "',381,1)";
+
+
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
