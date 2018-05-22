9082b8453f6cc743d76f622a54625b65fe41879d
skchavali <39451916+skchavali@users.noreply.github.com>
2018-05-22 12:41:04 -0700

    Update gdatastudio.php

--- gdatastudio.php
+++ gdatastudio.php
@@ -4,5 +4,6 @@ include 'include.php';
 
 ?>
 <htm>
+  <title> Google Data Studio </title>
   <iframe width="600" height="450" src="https://datastudio.google.com/embed/reporting/1yNz8mL8kbO0EfmujllFTo5ga8HR3lWb3/page/mwIS" frameborder="0" style="border:0" allowfullscreen></iframe>
 </htm>
