ec9e65da0540ac447b490c120bcc34182fc130fa
skchavali <39451916+skchavali@users.noreply.github.com>
2018-05-22 12:37:54 -0700

    Create gdatastudio.php

---
+++ gdatastudio.php
@@ -0,0 +1,8 @@
+<?php
+
+include 'include.php';
+
+?>
+<htm>
+  <iframe width="600" height="450" src="https://datastudio.google.com/embed/reporting/1yNz8mL8kbO0EfmujllFTo5ga8HR3lWb3/page/mwIS" frameborder="0" style="border:0" allowfullscreen></iframe>
+</htm>
