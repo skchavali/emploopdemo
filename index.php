9024465bc430fe2a7d0792ae1eca6885ed1eb725
skchavali <39451916+skchavali@users.noreply.github.com>
2019-06-20 10:13:52 -0700

    Update index.php

--- index.php
+++ index.php
@@ -20,4 +20,7 @@ include 'custcount.php';
 ?>
     
 <a href=insertactor.php> Insert Record </a>
+    
+    <script type="text/javascript">var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq || {widgetcode:"e8e3e0dddb33caa558323a7fbb51e7434739d8c8b2231e9086633bd6b65fa61716dd4fa5446ced601c90b4f14018801a", values:{},ready:function(){$zoho.salesiq.floatbutton.visible('hide');}};var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;s.src="https://salesiq.zoho.com/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);</script>
+    
     </htm>
