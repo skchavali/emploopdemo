df05862c39f497d729538efd451c8c97e2ae3290
skchavali <39451916+skchavali@users.noreply.github.com>
2019-08-24 14:13:30 -0700

    Add files via upload
    page view implementation

--- pageview.php
+++ pageview.php
@@ -2,7 +2,6 @@
 <title> Page View </title>
 <?php
 
-include 'include.php';
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $servername = $url["host"];
 $username = "b3c0dca2fea3fb";
@@ -17,16 +16,12 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$title= "Page View";
 $sql = "INSERT INTO acme_pageviews (page_title) VALUES ('"  . $title . "')";
 
 
 
 if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
 $conn->close();
