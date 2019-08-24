152f38eac1b75dc6c7a8e5e547747293ffd07141
skchavali <39451916+skchavali@users.noreply.github.com>
2019-08-24 14:20:44 -0700

    Add files via upload
    adding counter display

---
+++ pageviewcounter.php
@@ -0,0 +1,33 @@
+<html>
+<title> Page View Display </title>
+<?php
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$servername = $url["host"];
+$username = "b3c0dca2fea3fb";
+$password = "7200888a";
+$dbname = substr($url["path"],1);
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$title = "Home";
+$sql = "select count(*) from acme_pageview where page_title = ('"  . $title . "')";
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+
+$stmt->bind_result($count);
+
+while ($stmt->fetch()) {
+    echo "Number of Page Views " . $count ;
+}
+
+
+$stmt->close();
+
+?>
