c947fbc6675614d0c0d31730881241ddbe0e8fd3
skchavali <39451916+skchavali@users.noreply.github.com>
2019-08-24 20:45:02 -0700

    Add files via upload
    Popular posts

---
+++ popular.php
@@ -0,0 +1,29 @@
+<html>
+<title> Insert Success </title>
+<?php
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$servername = $url["host"];
+$username = "b3c0dca2fea3fb";
+$password = "7200888a";
+$dbname = substr($url["path"],1);
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$stmt = $conn->prepare("select count(*), page_title from acme_pageviews order by 1 desc");
+$stmt->execute();
+
+$stmt->bind_result($count, $title);
+
+while ($stmt->fetch()) {
+    echo "Page: " . $title . " has " . $count . " views";
+	echo "<br>";
+}
+
+$stmt->close();
+?>
