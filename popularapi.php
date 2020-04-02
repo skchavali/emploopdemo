218aa11e2eb0d5194ed5b2a863caac896b5d4e4b
skchavali <39451916+skchavali@users.noreply.github.com>
2020-04-02 15:28:05 -0700

    v7

--- popularapi.php
+++ popularapi.php
@@ -22,14 +22,10 @@ $result = $conn->query($sql);
 
 $data = array();
 
-if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        $data[] = $result->fetch_assoc();
+        $data[]=$row;
    }
-} else {
-    echo "0 results";
-}
 
 echo json_encode($data);  
  
