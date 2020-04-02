ca27012209d020f4f027df60efe427e690ce35fd
skchavali <39451916+skchavali@users.noreply.github.com>
2020-04-02 15:10:42 -0700

    v4

--- popularapi.php
+++ popularapi.php
@@ -10,26 +10,28 @@ $dbname = substr($url["path"],1);
 
 echo "page working?";
 
-$server = mysql_connect($servername, $username, $password);
-$connection = mysql_select_db($dbname, $servername);
+/ Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
 
-    $myquery = "select count(*) count, page_title from acme_pageviews group by page_title order by 1 desc"; 
-	$query = mysql_query($myquery);
-    
-    if ( ! $query ) {
-        echo mysql_error();
-		echo "no connection";
-        die;
-    }
-   
+$sql = "select count(*) count, page_title from acme_pageviews group by page_title order by 1 desc";
+$result = $conn->query($sql);
 
- $data = array();
-    
-    for ($x = 0; $x < mysql_num_rows($query); $x++) {
-        $data[] = mysql_fetch_assoc($query);
-    }
-    echo "print array";
-    echo json_encode($data);  
+$data = array();
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $data[] = $row;
+   }
+} else {
+    echo "0 results";
+}
+
+echo json_encode($data);  
  
 
 $conn->close();
